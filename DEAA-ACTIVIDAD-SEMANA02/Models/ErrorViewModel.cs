7fc61ca073dbe5a38d37d033d9e35a5e348dee53
RodrigoLupo <lupsue94@gmail.com>
2025-03-27 22:39:24 -0500

    Add initial project structure with views, layout, and configuration files

---
+++ DEAA-ACTIVIDAD-SEMANA02/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace DEAA_ACTIVIDAD_SEMANA02.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
